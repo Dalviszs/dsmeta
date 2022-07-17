099ab0bebd3d49b82855748a507b93a3d3dcc0b0
Dalvana Ribeiro <lopesribeirodalvana20@gmail.com>
2022-07-17 20:22:45 -0300

    API test

---
+++ backend/src/main/java/com/dalvisz/dsmeta/controllers/SaleController.java
@@ -0,0 +1,26 @@
+package com.dalvisz.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dalvisz.dsmeta.entities.Sale;
+import com.dalvisz.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping
+	public List<Sale> findSales(){
+		return service.findSales();
+	}
+	
+	
+
+}
